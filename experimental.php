d069b90d09eb2e3eae59038acb576a6b5078b4fa
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-08-03 11:52:10 +0200

    add changes structure and names

--- experimental.php
+++ experimental.php
@@ -1,85 +1,81 @@
 <?php
 
 // fw = for who
-function ordr_pz($pizzatype, $fw) {
+function order_pizza($pizzatype, $forWhom) {
 
-$type = $pizzatype;
-echo 'Creating new order... <br>';
-$toPrint = 'A ';
- $toPrint .= $pizzatype;
-$p = calc_cts($type);
-
+    $type = $pizzatype;
+    $toPrint = 'A ';
+    $toPrint .= $pizzatype;
+    $pizza = calc_cts($type);
     $address = 'unknown';
-    if($fw == 'koen')
-    {
-        $address = 'a yacht in Antwerp';
-    } elseif ($fw == 'manuele')
-    {
-        $address = 'somewhere in Belgium';
-    } elseif ($fw == 'students') {
-        $address = 'BeCode office';
+
+    if($forWhom == 'Koen') {
+        $address = ' a yacht in Antwerp';
+    } 
+    elseif ($forWhom == 'Manuele') {
+        $address = ' somewhere in Belgium';
+    } 
+    elseif ($forWhom == 'students') {
+        $address = ' BeCode office';
     }
 
-        $toPrint .=   ' pizza should be sent to ' . $fw . ". <br>The address: {$address}.";
-    echo $toPrint; echo '<br>';
-    echo'The bill is €'.$p.'.<br>';
+    $toPrint .= ' pizza should be sent to ' . $forWhom . '. <br>The address:' . $address . '.';
+    echo 'Creating new order...' . '<br>';
+    echo $toPrint . '<br>';
+    echo 'The bill is €'.$pizza.'.<br>';
+    echo 'Order finished.' . '<br><br>';
+};
 
+function total_price($pizza) {
+        return $pizza;
+};
 
+function test($pizza_type) {
+    echo 'Test: type is' . $pizza_type . '<br>';
+};
 
+function calc_cts($pizza_type) {
 
-    echo "Order finished.<br><br>";
-}
+    $cost = 'unknown';
 
-    function total_price($p) {
-            return $p;
+    if ($pizza_type == 'marguerita') {
+        $cost = 5;
     }
-
-        function test($p_type) {
-            echo "Test: type is {$p_type}. <br>";
+    else {
+        if ($pizza_type == 'golden') {
+            $cost = 100;
         }
 
-function calc_cts($p_type)
-{
-    $cst = 'unknown';
-
-    if ($p_type == 'marguerita') {
-        $cst = 5;
-    }
-    else
-        {
-        if ($p_type == 'golden')
-        {
-            $cst = 100;
-        }
-
-            if ($p_type == 'calzone')
-            {
-                $cst = 10;
+        if ($pizza_type == 'calzone') {
+                $cost = 10;
             }
 
-        if ($p_type == 'hawai') {
+        if ($pizza_type == 'hawai') {
             throw new Exception('Computer says no');
         }
     }
 
-    return $cst;
+    return $cost;
 }
 
-            function ordr_piz_all()
-            {
-            $test= 0;
-            ordr_pz('calzone', 'koen');
-            ordr_pz('marguerita', 'manuele');
+function order_pizza_all() {
+    
+    $test= 0;
+    order_pizza('calzone', 'koen');
+    order_pizza('marguerita', 'manuele');
 
-            ordr_pz('golden', 'students');
-            }
+    order_pizza('golden', 'students');
+};
 
 function make_Allhappy($do_it) {
-if ($do_it) {
-        ordr_piz_all();
-    } else {
-        // Should not do anything when false
-    }
-}
+    
+    if ($do_it) {
+            order_pizza_all();
+        } 
+    
+    else {
+            // Should not do anything when false
+        }
+};
 
 make_Allhappy(true);
